58374006f85ce916dab18104c8e72d063d43cfae
mxmdrg <72259411+mxmdrg@users.noreply.github.com>
2021-02-10 16:15:24 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php echo "Avtodar test";
+echo<<<html
+<center>hello</center>
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Avtodar</title>
+</head>
+<body>
+<h1>Avtodar test</h1>
+<p>If you can read this correctly, your website is functional!</p>
+</body>
+</html>
+html;
+?>
